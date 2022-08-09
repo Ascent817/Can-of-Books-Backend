521a15eb5b4014e6bd705f359fbc3007a66635ff
MARY <marymwihaki123@gmail.com>
2022-08-09 13:59:45 -0500

    added Post function

--- server.js
+++ server.js
@@ -20,12 +20,23 @@ app.get('/', (request, response) => {
   response.send('Ping successful.');
 });
 
-app.get('/books', GetBooks);
+app.get('/books', getBooks);
+app.post('/books', postBooks);
 
-async function GetBooks(request, response, next) {
+async function getBooks(request, response, next) {
   try {
     let data = await Book.find();
     response.status(200).send(data);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function postBooks(request, response, next) {
+  console.log(request.body);
+  try {
+    let newBook = await Book.create(request.body);
+    res.status(200).send(newBook);
   } catch (error) {
     next(error);
   }
